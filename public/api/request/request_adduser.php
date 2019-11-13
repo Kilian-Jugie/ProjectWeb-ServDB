a284417bb1c2c87052ec710b7c0846258ae5096a
Jesus <eliott.donatien@viacesi.fr>
2019-11-13 22:01:44 +0100

    Error correction

--- public/api/request/request_adduser.php
+++ public/api/request/request_adduser.php
@@ -8,24 +8,25 @@ class RequestAddUser extends Request {
         //$key = $params["p2"];
 
 		try {
-			$requete = singleton::getInstance()->prepare("CALL add_user(:_last_name,:_first_name,:_email,:_password,:_img_path,:_address,:_user_pseudo, :_newsletter, :_age, :_id_city)");
+			$response = $input['last_name'];
+			$requete = singleton::getInstance()->prepare("CALL add_user(:_last_name,:_first_name,:_email,:_password,:_img_path,:_address,:_user_pseudo,:_newsletter,:_age,:_id_city)");
 			$requete->bindValue('_last_name', $input['last_name'], PDO::PARAM_STR);
 			$requete->bindValue('_first_name', $input['first_name'], PDO::PARAM_STR);
 			$requete->bindValue('_email', $input['email'], PDO::PARAM_STR);
 			$requete->bindValue('_password', $input['password'], PDO::PARAM_STR);
 			$requete->bindValue('_img_path', $input['img_path'], PDO::PARAM_STR);
-			$requete->bindValue('_address', $input['address'], PDO::PARAM_STR);
+			$requete->bindValue('_address', $input['address'], PDO::PARAM_STR);	
 			$requete->bindValue('_user_pseudo', $input['user_pseudo'], PDO::PARAM_STR);
 			$requete->bindValue('_news_letter', $input['news_letter'], PDO::PARAM_STR);
 			$requete->bindValue('_age', $input['age'], PDO::PARAM_STR);
 			$requete->bindValue('_id_city', $input['id_city'], PDO::PARAM_STR);
 			$requete->execute();
 			$result = $requete->fetchAll();
-			if(!$result){
-				$response = "User been correctfully added";
-			}else{ 
-				$response = "Error while adding user";
-			}
+			// if(!$result){
+			// 	$response = "User been correctfully added";
+			// }else{ 
+			// 	$response = "Error while adding user";
+			// }
 			echo json_encode($response);			
 		}
 		catch (PDOException $e) {
