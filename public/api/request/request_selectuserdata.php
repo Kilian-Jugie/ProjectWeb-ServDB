76b237c341509b24a337172d780ebe2271b875bf
Jesus <eliott.donatien@viacesi.fr>
2019-11-13 23:22:10 +0100

    Error correction request User Data

--- public/api/request/request_selectuserdata.php
+++ public/api/request/request_selectuserdata.php
@@ -4,7 +4,8 @@
     class RequestSelectUserData extends Request {
         public function execute($params,$input) {
             try {
-                $requete = singleton::getInstance()->prepare("CALL select_user_data(".$params['p1'].")");
+                $requete = singleton::getInstance()->prepare("CALL select_user_data(:_user_email)");
+                $requete->bindValue(':_user_email', $params['p1'], PDO::PARAM_STR);
                 $requete->execute();
                 $result = $requete->fetchAll();
                 echo json_encode($result);
