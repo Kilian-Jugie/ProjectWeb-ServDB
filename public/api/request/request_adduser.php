ec144f5632faa47b3278dde85d7d1d8998e2e657
Jesus <eliott.donatien@viacesi.fr>
2019-11-13 22:38:54 +0100

    Error correction

--- public/api/request/request_adduser.php
+++ public/api/request/request_adduser.php
@@ -8,7 +8,7 @@ class RequestAddUser extends Request {
         //$key = $params["p2"];
 
 		try {
-			$requete = singleton::getInstance()->prepare("CALL add_user(:_last_name,:_first_name,:_email,:_password,:_img_path,:_address,:_user_pseudo,:_newsletter,:_age,:_id_city)");
+			$requete = singleton::getInstance()->prepare("CALL add_user(:_last_name,:_first_name,:_email,:_password,:_img_path,:_address,:_user_pseudo,:_news_letter,:_age,:_id_city)");
 			$requete->bindValue(':_last_name', $input['last_name'], PDO::PARAM_STR);
 			$requete->bindValue(':_first_name', $input['first_name'], PDO::PARAM_STR);
 			$requete->bindValue(':_email', $input['email'], PDO::PARAM_STR);
@@ -30,7 +30,7 @@ class RequestAddUser extends Request {
 		}
 		catch (PDOException $e) {
 			echo $e->getMessage();
-			echo json_encode($input);
+			echo json_encode($input);	
 		}
     }
 }
