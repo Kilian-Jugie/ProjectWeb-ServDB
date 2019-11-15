9db2a5bde4c8078fb5911d8ee169097b4278ce26
Kilian Jugie <kilian.jugie@viacesi.fr>
2019-11-15 10:14:14 +0100

    Test build

--- public/api/request/request_viewall.php
+++ public/api/request/request_viewall.php
@@ -1,17 +1,16 @@
 <?php
 
 require_once "request.php";
-require_once 'utils.php';
 
 class RequestViewAll extends Request {
     public function execute($params,$input) {
-        //$table = $params["p1"];
+        $table = $params["p1"];
 		//$key = $params["p2"];
 		try {
 			//$requete = singleton::getInstance()->prepare("SELECT * FROM ".$table);
 			//$requete->bindValue('_key', $key, PDO::PARAM_STR);
 			
-			
+			$requete = pdoprintf(singleton::getInstance(), "SELECT * FROM ".$table);
 			$requete->execute();
 			$result = $requete->fetchAll();
 			echo json_encode($result);
