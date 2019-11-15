4aac474283679873e52161846ab4d466b80611dd
Kilian Jugie <kilian.jugie@viacesi.fr>
2019-11-15 11:06:46 +0100

    sync

--- public/api/request/get/request_count_bde.php
+++ public/api/request/get/request_count_bde.php
@@ -9,28 +9,34 @@
      * count_event_by_campus p1 -> event
      */
 
-    class RequestCount extends Request {
+    class RequestCountBde extends Request {
         public function execute($params,$input) {
             try {
+                $ret = array();
+
                 $requete = pdoprintf(singleton::getInstance(), "CALL count_asso_campus(%i)", $params["p1"]);
                 $requete->execute();
                 $result = $requete->fetchAll();
-                echo json_encode($result);
+
+                $ret["asso"] = $result["COUNT(*)"];
 
                 $requete = pdoprintf(singleton::getInstance(), "CALL count_bde_member_campus(%i)", $params["p1"]);
                 $requete->execute();
                 $result = $requete->fetchAll();
-                echo json_encode($result);
+                
+                $ret["member"] = $result["COUNT(*)"];
 
                 $requete = pdoprintf(singleton::getInstance(), "CALL count_user_by_campus(%i)", $params["p1"]);
                 $requete->execute();
                 $result = $requete->fetchAll();
-                echo json_encode($result);
                 
+                $ret["follower"] = $result["COUNT(*)"];
+
                 $requete = pdoprintf(singleton::getInstance(), "CALL count_event_by_campus(%i)", $params["p1"]);
                 $requete->execute();
                 $result = $requete->fetchAll();
-                echo json_encode($result);
+                
+                $ret["event"] = $result["COUNT(*)"];
             }
             catch (PDOException $e) {
                 echo $e->getMessage();
