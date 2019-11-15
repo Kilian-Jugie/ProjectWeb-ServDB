634bcda75d2679e405e3142ef2ad1c73644a9fae
Kilian Jugie <kilian.jugie@viacesi.fr>
2019-11-15 15:27:45 +0100

    Missing file from previous commit

--- public/api/request/request_selectcampus.php
+++ public/api/request/request_selectcampus.php
@@ -5,7 +5,7 @@ require_once "request.php";
 class RequestSelectCampus extends Request {
     public function execute($params,$input) {
 		try {
-			$requete = singleton::getInstance()->prepare("CALL select_campus()");
+			$requete = singleton::getInstance()->prepare("CALL select_all_campus()");
 			$requete->execute();
 			$result = $requete->fetchAll();
             echo json_encode($result);
