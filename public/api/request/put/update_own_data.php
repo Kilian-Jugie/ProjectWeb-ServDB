fce82e0b4e2597264b634fa2ecf28b8d233e9eb1
Kilian Jugie <kilian.jugie@viacesi.fr>
2019-11-16 14:19:19 +0100

    Fixed: update_own_data argument password is incorrect

--- public/api/request/put/update_own_data.php
+++ public/api/request/put/update_own_data.php
@@ -6,9 +6,9 @@
         public function execute($params,$input) {
             $accountId = $params['p1'];
             try {
-                $requete = pdoprintf(singleton::getInstance(), 'CALL update_own_account(%i, %s, %s, %s, %s, %s, %s, %i, %i, %i)',
-                    $accountId, $input['firstname'], $input['email'], $input['password'], $input['thumbnail'],
-                    $input['address'], $input['pseudo'], $input['newsletter'], $input['age'], $input['campus_id']);
+                $requete = pdoprintf(singleton::getInstance(), 'CALL update_own_account(%i, %s, %s, %s, %s, %s, %i, %i, %i)',
+                    $accountId, $input['firstname'], $input['email'], $input['thumbnail'], $input['address'], $input['pseudo'], 
+                    $input['newsletter'], $input['age'], $input['campus_id']);
                 $requete->execute();
                 $result = $requete->fetchAll();
 
