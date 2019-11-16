110a20972dc1b86cd5c5ef8bf143ece19fee87c9
Kilian Jugie <kilian.jugie@viacesi.fr>
2019-11-16 16:26:31 +0100

    Fixed: wrong parameters number for update_stock

--- public/api/request/put/update_stock.php
+++ public/api/request/put/update_stock.php
@@ -5,7 +5,7 @@
     class RequestUpdateStock extends Request {
         public function execute($params,$input) {
             try {
-                $requete = pdoprintf(singleton::getInstance(), "CALL update_stock_data(%i)",
+                $requete = pdoprintf(singleton::getInstance(), "CALL update_stock_data(%i, %i)",
                     $params['p1'], $input['quantity']);
                 $requete->execute();
                 $result = $requete->fetchAll();
