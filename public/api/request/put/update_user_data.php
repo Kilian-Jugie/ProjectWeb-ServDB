2d024cafc62de619882753fafa8d24d78ddc7494
Kilian Jugie <kilian.jugie@viacesi.fr>
2019-11-16 14:28:20 +0100

    Fixed: update_user_data args nb

--- public/api/request/put/update_user_data.php
+++ public/api/request/put/update_user_data.php
@@ -6,7 +6,7 @@
         public function execute($params,$input) {
             $accountId = $params['p1'];
             try {
-                $requete = pdoprintf(singleton::getInstance(), 'CALL update_user_data(%i, %i)',
+                $requete = pdoprintf(singleton::getInstance(), 'CALL update_user_data(%i, %i, %i)',
                     $input['id_role'], $input['id_campus'], $accountId);
                 $requete->execute();
                 $result = $requete->fetchAll();
