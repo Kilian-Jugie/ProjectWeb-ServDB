392add5bd4e3dc5d9e48b12a486a44bf3937e854
Kilian Jugie <kilian.jugie@viacesi.fr>
2019-11-17 23:13:15 +0100

    Added: all_size

--- public/api/API.php
+++ public/api/API.php
@@ -22,6 +22,7 @@ require_once "request/get/all_product_type.php";
 require_once "request/get/own_event.php";
 require_once "request/get/get_stock.php";
 require_once "request/get/all_stock.php";
+require_once "request/get/all_size.php";
 
 require_once "request/post/add_campus.php";
 require_once "request/post/add_event.php";
@@ -95,6 +96,7 @@ class API
 		ActionGet::getInstance()->addRequest(new RequestOwnEvent("own_event"));
 		ActionGet::getInstance()->addRequest(new RequestGetStock("get_stock"));
 		ActionGet::getInstance()->addRequest(new RequestAllStock("all_stock"));
+		ActionGet::getInstance()->addRequest(new RequestAllSize("all_size"));
 
 		self::registerActionForMethod('POST', ActionPost::getInstance());
 		ActionPost::getInstance()->addRequest(new RequestAddUser("add_user"));
