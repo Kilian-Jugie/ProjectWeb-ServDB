52092da864fc5d87fe18aaebe565dff0963b9fcb
Kilian Jugie <kilian.jugie@viacesi.fr>
2019-11-17 16:23:00 +0100

    Fixed: update_product_cart

--- public/api/request/put/update_product_cart.php
+++ public/api/request/put/update_product_cart.php
@@ -5,7 +5,7 @@
     class RequestUpdateProductCart extends Request {
         public function execute($params,$input) {
             try {
-                $requete = pdoprintf(singleton::getInstance(), "CALL update_product_type(%i, %i, %i)",
+                $requete = pdoprintf(singleton::getInstance(), "CALL update_product_cart(%i, %i, %i)",
                     $params['p1'], $input['stock_id'], $input['quantity']);
                 $requete->execute();
                 $result = $requete->fetchAll();
