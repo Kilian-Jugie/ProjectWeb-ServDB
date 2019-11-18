e9c5b3a935f642fccfd6786970e7c5414c72ae1b
Kilian Jugie <kilian.jugie@viacesi.fr>
2019-11-18 14:54:40 +0100

    Updated add_event

--- public/api/request/post/add_event.php
+++ public/api/request/post/add_event.php
@@ -5,9 +5,9 @@
     class RequestAddEvent extends Request {
         public function execute($params,$input) {
             try {
-                $requete = pdoprintf(singleton::getInstance(), 'CALL add_event(%s, %s, %s, %s, %i, %i, %i, %i, %s)',
+                $requete = pdoprintf(singleton::getInstance(), 'CALL add_event(%s, %s, %s, %s, %i, %i, %i, %s)',
                     $input['input_title'], $input['input_content'], $input['input_date'], $input['input_end_date'],
-                    $input['input_cost'], $input['input_id_user'], $input['input_id_occurence'], $input['input_id_status'],
+                    $input['input_cost'], $input['input_id_user'], $input['input_id_occurence'],
                     $input['input_image_json_file']);
                 $requete->execute();
                 $result = $requete->fetchAll();
