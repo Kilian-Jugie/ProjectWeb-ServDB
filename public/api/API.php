826dac191ab529ab29fa9939a1be006f75dbff2d
Kilian Jugie <kilian.jugie@viacesi.fr>
2019-11-18 02:46:55 +0100

    Added: all_status

--- public/api/API.php
+++ public/api/API.php
@@ -26,6 +26,7 @@ require_once "request/get/all_size.php";
 require_once "request/get/all_order.php";
 require_once "request/get/request_actif_event_data.php";
 require_once "request/get/picture_pub.php";
+require_once "request/get/all_status.php";
 
 require_once "request/post/add_campus.php";
 require_once "request/post/add_event.php";
@@ -104,6 +105,7 @@ class API
 		ActionGet::getInstance()->addRequest(new RequestAllOrder("all_order"));
 		ActionGet::getInstance()->addRequest(new RequestActifEventData("actif_event_data"));
 		ActionGet::getInstance()->addRequest(new RequestPicturePub("picture_pub"));
+		ActionGet::getInstance()->addRequest(new RequestAllStatus("all_status"));
 
 		self::registerActionForMethod('POST', ActionPost::getInstance());
 		ActionPost::getInstance()->addRequest(new RequestAddUser("add_user"));
