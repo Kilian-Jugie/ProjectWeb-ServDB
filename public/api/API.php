afd715ff9a0ce6b7f70a691211e1a649451c4b3a
Kilian Jugie <kilian.jugie@viacesi.fr>
2019-11-18 03:08:35 +0100

    Added: user_per_event

--- public/api/API.php
+++ public/api/API.php
@@ -30,6 +30,7 @@ require_once "request/get/all_status.php";
 require_once "request/get/all_reaction.php";
 require_once "request/get/all_role.php";
 require_once "request/get/all_user.php";
+require_once "request/get/user_per_event.php";
 
 require_once "request/post/add_campus.php";
 require_once "request/post/add_event.php";
@@ -112,6 +113,7 @@ class API
 		ActionGet::getInstance()->addRequest(new RequestAllReaction("all_reaction"));
 		ActionGet::getInstance()->addRequest(new RequestAllRole("all_role"));
 		ActionGet::getInstance()->addRequest(new RequestAllUser("all_user"));
+		ActionGet::getInstance()->addRequest(new RequestUserPerEvent("user_per_event"));
 
 		self::registerActionForMethod('POST', ActionPost::getInstance());
 		ActionPost::getInstance()->addRequest(new RequestAddUser("add_user"));
